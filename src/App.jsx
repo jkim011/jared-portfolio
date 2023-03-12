eebacc09c62d132517aae04ca91873fc7303f4c9
Jared Kim <jaredkim01@yahoo.com>
2023-03-12 15:11:56 -0500

    functioning nav bar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header'
+import Header from './components/Header';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
 
 function App() {
 
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <>
+          <Header />
+          <Routes>
+            <Route 
+              path='/'
+              element={<Home />}
+            />
+            <Route 
+              path='/portfolio'
+              element={<Portfolio />}
+            />
+            <Route 
+              path='/resume'
+              element={<Resume />}
+            />
+            <Route 
+              path='/contact'
+              element={<Contact />}
+            />
+          </Routes>
+        </>       
+      </Router>
+      
       
     </div>
   )
