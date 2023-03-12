3afd4db3fc60055fb34c3698046f6dc4a03e8fbc
Jared Kim <jaredkim01@yahoo.com>
2023-03-12 14:31:39 -0500

    imported bootstrap/nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Jared Kim</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Github</Nav.Link>
+            <Nav.Link href="#pricing">LinkedIn</Nav.Link>
+            <Nav.Link href="#pricing">Email</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Home</Nav.Link>
+            <Nav.Link href="#memes">Portfolio</Nav.Link>
+            <Nav.Link href="#memes">Resume</Nav.Link>
+            <Nav.Link href="#memes">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
