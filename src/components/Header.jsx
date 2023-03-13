9c7ef43ce2af02e1e5a7be463a80a2764893d455
Jared Kim <jaredkim01@yahoo.com>
2023-03-12 21:31:50 -0500

    finished homepage/aboutme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,20 +10,26 @@ import emailLogo from '../assets/logo-images/emailLogo.png'
 
 function Header() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="nav-margins">
       <Container>
         <Navbar.Brand href="#home">Jared Kim</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto ">
-            <Nav.Link href="https://github.com/jkim011/" target="_blank" rel="noreferrer"><img className="social-links" src={githubLogo} /></Nav.Link>
-            <Nav.Link href="https://www.linkedin.com/in/jared-kim/" target="_blank" rel="noreferrer"><img className="social-links" src={linkedinLogo} /></Nav.Link>
-            <Nav.Link href="mailto:jaredkim011@gmail.com" ><img className="social-links" src={emailLogo} /></Nav.Link>
+            {/* <Nav.Link href="https://github.com/jkim011/" target="_blank" rel="noreferrer">
+              <img className="social-links" src={githubLogo} />
+            </Nav.Link>
+            <Nav.Link href="https://www.linkedin.com/in/jared-kim/" target="_blank" rel="noreferrer">
+              <img className="social-links" src={linkedinLogo} />
+            </Nav.Link>
+            <Nav.Link href="mailto:jaredkim011@gmail.com" >
+              <img className="social-links" src={emailLogo} />
+            </Nav.Link> */}
           </Nav>
           <Nav>
             <Nav.Link as={Link} to='/'>Home</Nav.Link>
-            <Nav.Link as={Link} to='/portfolio'>Portfolio</Nav.Link>
-            <Nav.Link as={Link} to='/resume'>Resume</Nav.Link>
+            <Nav.Link as={Link} to='/skills'>Skills</Nav.Link>
+            <Nav.Link as={Link} to='/portfolio'>Portfolio</Nav.Link>           
             <Nav.Link as={Link} to='/contact'>Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import Contact from "./Contact";
+import portfolioPic from '../assets/portfolioPic.jpg'
+import githubLogo from '../assets/logo-images/githubLogo.png';
+import linkedinLogo from '../assets/logo-images/linkedinLogo.png';
+import emailLogo from '../assets/logo-images/emailLogo.png'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h3 className="intro">
+        Hello! My name is <br/>
+        <span style={{fontSize:"200%", fontWeight:"bolder"}}>Jared Kim</span> <br/>
+        Full Stack Web Developer
+      </h3>
+      <div className="profile">
+        <img src={portfolioPic} className='portfolioPic'/>
+        <div className="profile-links">
+          <a href="" className="textDecNone"><p>Download Resume</p></a>
+          <Link to='/contact' className="textDecNone"><p>Let's Chat!</p></Link>
+          <a href="https://github.com/jkim011/" target="_blank" rel="noreferrer"><img className="social-links" src={githubLogo} /></a>
+          <a href="https://www.linkedin.com/in/jared-kim/" target="_blank" rel="noreferrer"><img className="social-links" src={linkedinLogo} /></a>
+          <a href="mailto:jaredkim011@gmail.com"><img className="social-links" src={emailLogo} /></a>
+        </div>
+      </div>
+      <div className="about-me">
+        <h2>About Me</h2>
+        <p className="about-me-text">
+          Hi everyone, I'm Jared, a full stack web developer based in Leander, Texas. 
+          My favorite part about web development is how it is always changing and there is always something new to learn, so it will never get stale. 
+          I enjoy building new web applications that can better people's lives or help them showcase their own work and talents.
+          <br/>
+          Prior to web development, I was a martial arts instructor for almost 4 years, and carry many of the skills I learned 
+          into my new career as a developer, such as attention to detail, organization, time efficiency, and teamwork.
+          Training martial arts is still one of my hobbies as it helps keeps me disciplined and focused. I also enjoy video games
+          and I am a massive hip hop head. 
+        </p>
+      </div>
+    </div>
   )
 }
 
