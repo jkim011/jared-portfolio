2fcbf6feb9a545e9d233a4bfe507213aa289f7d8
Jared Kim <jaredkim01@yahoo.com>
2023-03-14 17:10:41 -0500

    finished skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -2,7 +2,32 @@ import React from "react";
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <div>
+      <h2>My Skills</h2> <br/>
+      <div className="skills-section">
+        <div className="frontend-skills">
+          <h3>Front End Skills</h3>
+          <ul className="skills-list">
+            <li>React</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>Bootstrap</li>
+          </ul>
+        </div>
+        <div className="backend-skills">
+          <h3>Back End Skills</h3>
+          <ul className="skills-list">
+            <li>NodeJs</li>
+            <li>Express</li>
+            <li>MySql</li>
+            <li>MongoDB</li>
+            <li>Insomnia</li>
+          </ul>
+        </div>
+      </div>
+      <a href="" className="textDecNone"><h5>Download my resume</h5></a>
+    </div>
   )
 }
 
