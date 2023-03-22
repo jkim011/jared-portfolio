3213384b8283b061e8016a6cfe94beaa2a4db204
Jared Kim <jaredkim01@yahoo.com>
2023-03-22 16:04:04 -0500

    contact page done, small style changes

--- src/components/SingleProject.jsx
+++ src/components/SingleProject.jsx
@@ -1,25 +1,9 @@
 import React from "react";
-
-import githubLogo from '../assets/logo-images/githubLogo.png';
-
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 function SingleProject(props) {
   return (
-    // <div className="card project-card "> 
-    //   <img src={props.imageSrc} className="card-img project-border" alt="..."/>
-    //   <div className="card-img-overlay">
-    //     <a href={props.projectLink} className="textDecNone project-card-text" target="_blank" rel="noreferrer">
-    //       <h2 className="card-title">{props.projectTitle}</h2>
-    //     </a>
-    //     <a href={props.projectGithub} className="textDecNone project-card-text" target="_blank" rel="noreferrer">
-    //       <img src={githubLogo} alt="GitHub Link" className="githubLogo"/>
-    //     </a>
-    //     <p className="card-text" style={{color:"black"}}>{props.projectDescription}</p>
-    //   </div>
-    // </div>
-
     <Card className="box-shadow" border="danger" style={{ width: '500px', marginBottom:"40px", marginLeft:"20px", marginRight:"20px", backgroundColor:"#FCFFF7" }} >      
       <Card.Body>
         <Card.Title><a href={props.projectLink} style={{fontWeight:"bolder", fontSize:"110%"}} className="textDecNone project-card-text" target="_blank" rel="noreferrer">{props.projectTitle}</a></Card.Title>
