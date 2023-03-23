dc0888d048784f100f9d884f13cae34ff7aed7bc
Jared Kim <jaredkim01@yahoo.com>
2023-03-22 22:24:33 -0500

    took out hashrouter in main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -5,9 +5,9 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    {/* <HashRouter> */}
       <App />
-    </HashRouter>
+    {/* </HashRouter> */}
   </React.StrictMode>,
   document.getElementById('root')
 )
