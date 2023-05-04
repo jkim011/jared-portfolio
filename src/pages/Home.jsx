02008a974854cab27601adfec661d56ff429f2b3
Jared Kim <jaredkim01@yahoo.com>
2023-05-03 23:07:36 -0500

    updated resume

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ function Home() {
             As a former martial arts instructor, I gained a handful of soft skills 
             such as: attention to detail, organization, time efficiency, and teamwork.
             My <Link to="/skills" className="textDecNone links">technical skills</Link> are listed in my <a href={resumePdf} download="jareds-coding-resume.pdf" className="textDecNone links">resume. </a> 
-            Training in martial arts is still a hobby of mine since it keeps me disciplined and focused. To wind down I enjoy playing video games
+            Training in martial arts is still a hobby of mine since it keeps me disciplined and focused. I also enjoy playing video games
             and listening to music, mainly hip-hop and rock. 
             Thanks for checking out my page! <Link to='/contact' className="textDecNone links">Lets chat!</Link>               
           </p>
